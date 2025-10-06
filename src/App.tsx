07c0a70d943849decf745ebadbf74feb825a0bc6
MaximusProject <maximiliano098345@gmail.com>
2025-10-06 20:33:13 -0300

    fix: Resolver conflictos de merge y corregir App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { PinAuthProvider } from "@/hooks/usePinAuth";
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
->>>>>>> 806ce752b2ffd01196fc27e4e10850998b243736
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -15,7 +11,6 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-<<<<<<< HEAD
     <PinAuthProvider>
       <TooltipProvider>
         <Toaster />
@@ -23,7 +18,6 @@ const App = () => (
         <BrowserRouter basename="/CalendarioSJT">
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/CalendarioSJT/" element={<Navigate to="/" replace />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
@@ -33,20 +27,3 @@ const App = () => (
 );
 
 export default App;
-=======
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
->>>>>>> 806ce752b2ffd01196fc27e4e10850998b243736
